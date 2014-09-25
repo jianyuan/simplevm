578a57b4e3366918d182d692256bd6c19d4965e8
Jian Yuan <jianyuan@gmail.com>
2014-09-25 07:54:13 +0100

    More test cases

--- instruction_test.go
+++ instruction_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+func TestEncodeInstructionAndOverflow(t *testing.T) {
+	instr_actual := &Instruction{Operation(0x1), Data(0x2), Data(0x3), Data(0x4)}
+	assert.Equal(t, 0x1234, instr_actual.EncodeInstruction())
+	instr_actual_2 := &Instruction{Operation(0x11), Data(0x22), Data(0x33), Data(0x44)}
+	assert.Equal(t, 0x1234, instr_actual_2.EncodeInstruction())
+}
+
+func TestDecodeInstruction(t *testing.T) {
+	instr_expected := &Instruction{Operation(0x1), Data(0x2), Data(0x3), Data(0x4)}
+	var instr_code InstructionCode = 0x1234
+	instr_actual := DecodeInstruction(instr_code)
+	assert.Equal(t, instr_expected, instr_actual)
+}
+
 func TestEncodeDecodeInstruction(t *testing.T) {
 	instr_in := &Instruction{Operation(0x1), Data(0x2), Data(0x3), Data(0x4)}
 	instr_code := instr_in.EncodeInstruction()
