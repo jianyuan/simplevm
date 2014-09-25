afe3dae30e4da195ae5e6eb2297247a5e6f0c08a
Jian Yuan <jianyuan@gmail.com>
2014-09-25 08:36:10 +0100

    Add mul, div and mov instructions

--- examples/main.go
+++ examples/main.go
@@ -10,12 +10,14 @@ import (
 
 func main() {
 	program := [...]simplevm.Instruction{
-		{simplevm.OpcodeLoad, 0, 5, 0},  // ldr r0 5
-		{simplevm.OpcodeLoad, 1, 6, 0},  // ldr r1 6
-		{simplevm.OpcodeLoad, 2, 2, 0},  // ldr r2 2
+		{simplevm.OpcodeLoad, 0, 8, 0},  // r0 := 8
+		{simplevm.OpcodeLoad, 1, 6, 0},  // r1 := 6
+		{simplevm.OpcodeLoad, 2, 2, 0},  // r2 := 2
 		{simplevm.OpcodeAdd, 3, 0, 1},  // r3 := r0 + r1
 		{simplevm.OpcodeSub, 3, 3, 2},  // r3 := r3 - r2
-		{simplevm.OpcodeDisp, 3, 0, 0}, // disp r2
+		{simplevm.OpcodeDiv, 3, 3, 1},  // r3 := r3 / r1
+		{simplevm.OpcodeMul, 3, 3, 0},  // r3 := r3 * r1
+		{simplevm.OpcodeDisp, 3, 0, 0}, // disp r3
 		{simplevm.OpcodeHalt, 0, 0, 0}, // halt
 	}
 
--- instructions.go
+++ instructions.go
@@ -7,8 +7,11 @@ import (
 const (
 	OpcodeHalt Operation = iota
 	OpcodeLoad
+	OpcodeMov
 	OpcodeAdd
 	OpcodeSub
+	OpcodeMul
+	OpcodeDiv
 	OpcodeDisp
 )
 
@@ -20,6 +23,10 @@ func load(vm *VM, instr *Instruction) {
 	vm.Registers[instr.Arg1] = instr.Arg2
 }
 
+func mov(vm *VM, instr *Instruction) {
+	vm.Registers[instr.Arg1] = vm.Registers[instr.Arg2]
+}
+
 func add(vm *VM, instr *Instruction) {
 	vm.Registers[instr.Arg1] = vm.Registers[instr.Arg2] + vm.Registers[instr.Arg3]
 }
@@ -28,14 +35,25 @@ func sub(vm *VM, instr *Instruction) {
 	vm.Registers[instr.Arg1] = vm.Registers[instr.Arg2] - vm.Registers[instr.Arg3]
 }
 
+func mul(vm *VM, instr *Instruction) {
+	vm.Registers[instr.Arg1] = vm.Registers[instr.Arg2] * vm.Registers[instr.Arg3]
+}
+
+func div(vm *VM, instr *Instruction) {
+	vm.Registers[instr.Arg1] = vm.Registers[instr.Arg2] / vm.Registers[instr.Arg3]
+}
+
 func disp(vm *VM, instr *Instruction) {
-	fmt.Printf("r%d: %d", instr.Arg1, vm.Registers[instr.Arg1])
+	fmt.Printf("r%d: %d\n", instr.Arg1, vm.Registers[instr.Arg1])
 }
 
 var Instructions = map[Operation]func(*VM, *Instruction){
 	OpcodeHalt: halt,
 	OpcodeLoad: load,
+	OpcodeMov:  mov,
 	OpcodeAdd:  add,
 	OpcodeSub:  sub,
+	OpcodeMul:  mul,
+	OpcodeDiv:  div,
 	OpcodeDisp: disp,
 }
